d896a0210b3e27260d60624142f04766c98d3a29
Naser Mearti <nevilart@gmail.com>
2020-01-14 07:14:10 +0200

    U20200114
    Teapot update 1 (Not complete yet)

--- __init__.py
+++ __init__.py
@@ -18,9 +18,9 @@
 
 bl_info = {
 	"name": "BsMax",
-	"description": "BsMax Updated on 13.january.2020",
+	"description": "BsMax Updated on 14.january.2020",
 	"author": "Naser Merati (Nevil)",
-	"version": (0, 1, 0, 20200113),
+	"version": (0, 1, 0, 20200114),
 	"blender": (2, 80, 0),# 2.80~2.83
 	#"location": "Every where",
 	#"warning": "", # used for warning icon and text in addons panel
--- primitive/teapot.py
+++ primitive/teapot.py
@@ -1,9 +1,9 @@
 import bpy
 from mathutils import Vector
-from math import sin, cos, pi, radians
-from bsmax.curve import Spline, Bezier_point
-from bsmax.math import point_on_vector
-from primitive.primitive import CreatePrimitive, PrimitiveGeometryClass
+from math import sin,cos,pi,radians
+from bsmax.curve import Spline,Bezier_point
+from bsmax.math import point_on_vector,get_distance
+from primitive.primitive import CreatePrimitive,PrimitiveGeometryClass
 
 def get_path(part):
 	body_path = Spline(None)
@@ -18,18 +18,22 @@ def get_path(part):
 		points = (((0,0,1.575),(0,0,1.575),(0.400656,0,1.56443)),
 			((0.00236605,0,1.43537),(0.1,0,1.35),(0.211611,0,1.27556)),
 			((0.657876,0,1.26831),(0.65,0,1.2),(0.65,0,1.2)))
-	elif part == 'spout':
-		points = (((0.850668,1.23725,0.507198),(0.850668,1.23725,0.507198),(1.38542,1.23725,0.549446)),
-			((1.19781,1.23725,1.09924),(1.54661,1.23725,1.23185),(1.54661,1.23725,1.23185)))
-	elif part == 'handle_path':
-		points = (((-0.775849,0,1.07083),(-0.775849,0,1.07083),(-1.09616,0,1.06615)),
-			((-1.40217,0,1.09104),(-1.4273,0,0.922712),(-1.45329,0,0.649498)),
-			((-1.18858,0,0.50098),(-0.917913,0,0.328547),(-0.917913,0,0.328547)))
-	elif part == 'handle_shape':
-		points = (((-0.803255,0.108984,1.03168),(-0.803255,0.108984,1.06697),(-0.803255,0.108984,1.10225)),
-			((-0.803255,0.0601358,1.12498),(-0.803255,0,1.12498),(-0.803255,-0.0601359,1.12498)),
-			((-0.803255,-0.108984,1.10225),(-0.803255,-0.108984,1.06697),(-0.803255,-0.108984,1.03168)),
-			((-0.803255,-0.0601359,1.01323),(-0.803255,0,1.01323),(-0.803255,0.0601358,1.01323)))
+	elif part == 'spout_a':
+		points = (((0.85,0,0.7125),(0.85,0,0.7125),(1.30583,0,0.724497)),
+			((1.15645,0,1.0605),(1.35,0,1.2),(1.39425,0,1.23295)),
+			((1.45212,0,1.24122),(1.4,0,1.2),(1.4,0,1.2)))
+	elif part == 'spout_b':
+		points = (((0.848828,0,0.3),(0.848828,0,0.3),(1.5298,0,0.39321)),
+			((1.22074,0,1.03193),(1.65,0,1.2),(1.70914,0,1.22046)),
+			((1.78246,0,1.27581),(1.6,0,1.2),(1.6,0,1.2)))
+	elif part == 'handle_a':
+		points = (((-0.75,0,1.125),(-0.75,0,1.125),(-0.999939,0,1.12514)),
+			((-1.51932,0,1.16786),(-1.5,0,0.9),(-1.47991,0,0.621563)),
+			((-1.2955,0,0.456572),(-0.95,0,0.3),(-0.95,0,0.3)))
+	elif part == 'handle_b':
+		points = (((-0.8,0,1.0125),(-0.8,0,1.0125),(-1.01763,0,0.999294)),
+			((-1.34732,0,1.04424),(-1.35,0,0.9),(-1.35272,0,0.753523)),
+			((-1.19108,0,0.528823),(-1,0,0.45),(-1,0,0.45)))
 	for p in points:
 		newbp = Bezier_point(None)
 		newbp.handle_left = Vector((p[0]))
@@ -38,27 +42,53 @@ def get_path(part):
 		body_path.bezier_points.append(newbp)
 	return body_path
 
+def get_ring(point1, point2, scale):
+	ring = Spline(None)
+	dist = get_distance(point1, point2)
+	d = Vector((0,dist,0))
+	p1 = Bezier_point(None)
+	p2 = Bezier_point(None)
+	p1.handle_left = point1+d
+	p1.co = point1
+	p1.handle_right = point1-d
+	ring.bezier_points.append(p1)
+	p2.handle_left = point2-d
+	p2.co = point2
+	p2.handle_right = point2+d
+	ring.bezier_points.append(p2)
+	ring.use_cyclic_u = True
+	return ring
+
 def get_verts(spline, ssegs, scale):
 	verts = []
-	for index in range(len(spline.bezier_points)-1):
-		a,b,c,d = spline.get_segment(index)
-		for j in range(ssegs):
-			t = 1/ssegs * j
-			newvert = point_on_vector(a,b,c,d,t)
-			verts.append(newvert * scale)
-		if index == len(spline.bezier_points)-2:
-			verts.append(d * scale)
+	if spline.use_cyclic_u:
+		for index in range(len(spline.bezier_points)):
+			a,b,c,d = spline.get_segment(index)
+			for j in range(ssegs):
+				t = (1/ssegs)*j
+				newvert = point_on_vector(a,b,c,d,t)
+				verts.append(newvert*scale)
+	else:
+		for index in range(len(spline.bezier_points)-1):
+			a,b,c,d = spline.get_segment(index)
+			for j in range(ssegs):
+				t = (1/ssegs)*j
+				newvert = point_on_vector(a,b,c,d,t)
+				verts.append(newvert*scale)
+			if index == len(spline.bezier_points)-2:
+				verts.append(d*scale)
 	return verts
 
 def get_teapot_mesh(radius,csegs,body,handle,spout,lid):
 	verts,edges,faces = [],[],[]
 	cs = csegs*4
 	step = (pi*2)/cs
-	#print("-------------------------------------")
-	if body:
-		""" Create body vertexes """
-		body_path = get_path('body')
-		sides = get_verts(body_path, csegs, radius)
+
+	def create_body(line, flip):
+		f = len(verts) # First vertex of element
+		""" Create vertexes """
+		path = get_path(line)
+		sides = get_verts(path, csegs, radius)
 		l = len(sides)-1
 		verts.append(sides[0])
 		for i in range(1,l):
@@ -69,12 +99,20 @@ def get_teapot_mesh(radius,csegs,body,handle,spout,lid):
 				Y = cos(d)*x
 				Z = sides[i].z
 				verts.append([X,Y,Z])
-		""" first body triangle faces """
+		""" first triangle faces """
 		for i in range(cs):
-			faces.append((0,i,i+1))
+			if flip:
+				faces.append((f,f+i,f+i+1))
+			else:
+				faces.append((f+i+1,f+i,f))
+		if flip:
+			faces.append((f,f+cs,f+1))
+		else:
+			faces.append((f+1,f+cs,f))
+				
 		faces.append((0,cs,1))
-		""" body quad faces """
-		f = 1 # First vertex of element
+		""" faces """
+		f += 1 # First vertex of element
 		for i in range(l-2):
 			for j in range(cs):
 				a = f+j+(i*cs)
@@ -86,50 +124,49 @@ def get_teapot_mesh(radius,csegs,body,handle,spout,lid):
 					b = f+i*cs
 					c = a+1
 					d = a+cs
-				faces.append((d,c,b,a))
+				if flip:
+					faces.append((d,c,b,a))
+				else:
+					faces.append((a,b,c,d))
 
-	if lid:
-		f = len(verts) # First vertex of element
-		""" create lid vertexes """
-		lid_path = get_path('lid')
-		sides = get_verts(lid_path, csegs, radius)
-		l = len(sides)-1
-		verts.append(sides[0])
-		for i in range(1,len(sides)):
-			x = sides[i].x
-			for j in range(cs):
-				d = j*step
-				X = sin(d)*x 
-				Y = cos(d)*x
-				Z = sides[i].z
-				verts.append([X,Y,Z])
-		""" first lid triangle faces """
-		for i in range(cs):
-			faces.append((f+i+1,f+i,f))
-		faces.append((f+1,f+cs,f))
-		""" lid quad faces """
-		f += 1
-		for i in range(l-1):
-			print(i)
-			for j in range(cs):
-				a = f+j+(i*cs)
-				if j < cs-1:
+	def create_pipe(line1, line2, flip):
+		f = len(verts)
+		""" create vertexes """
+		spline1 = get_path(line1)
+		spline2 = get_path(line2)
+		v1 = get_verts(spline1,csegs,radius)
+		v2 = get_verts(spline2,csegs,radius)
+		for p1,p2 in zip(v1,v2):
+			ring = get_ring(p1,p2,1)
+			vert = get_verts(ring,csegs,1)
+			for v in vert:
+				verts.append(v)
+		""" faces """
+		ssegs = csegs*2
+		for i in range(len(v1)-1):
+			for j in range(ssegs):
+				a = f+i*ssegs+j
+				if j < ssegs-1:
 					b = a+1
-					c = a+cs+1
+					c = a+ssegs+1
 					d = c-1
-				else: 
-					b = f+i*cs
-					c = a+1
-					d = c+cs-1
-				faces.append((a,b,c,d))
-	
-	""" create handel vertexes """
-	if handle:
-		f = len(verts)
+				else:
+					b = f+i*ssegs
+					c = b+ssegs
+					d = a+ssegs
+				if flip:
+					faces.append((d,c,b,a))
+				else:
+					faces.append((a,b,c,d))
 
-	""" create spout vertexes """
+	if body:
+		create_body('body', True)
+	if lid:
+		create_body('lid', False)
+	if handle:
+		create_pipe("handle_a", "handle_b", True)
 	if spout:
-		f = len(verts)
+		create_pipe("spout_a", "spout_b", False)
 
 	return verts,edges,faces
 
